c43d73334c479016fca880118c0498763a14717b
Jagaddeesh Krishna S Nunna <66049652+JagadeeshKrishnaSNunna@users.noreply.github.com>
2021-08-24 10:04:35 +0530

    Update runScript.sh

--- runScript.sh
+++ runScript.sh
@@ -6,6 +6,6 @@ echo "tsunami image build successful..!"
 
 echo "initiating tsunami scan..!"
 
-#docker run --rm -e IP=$1 -v $PWD:/home/tsunami --name tsunami tsunami:latest
+docker run --rm -e IP=$1 -v $PWD:/home/tsunami --name tsunami tsunami:latest
 
 echo "scan complete..!" 
